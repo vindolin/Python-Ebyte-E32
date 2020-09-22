dfa8b51887f61ef772d30bde9dce68026fa77a31
Thomas Schüßler <v@atomar.de>
2020-09-22 15:14:54 +0100

    WIP

--- test_raspberrypi.py
+++ test_raspberrypi.py
@@ -11,20 +11,16 @@ ser = serial.Serial('/dev/serial0')
 ebyte = EbyteRaspberryPi(ser, PIN_M0, PIN_M1, PIN_AUX)
 
 print(ebyte.read_version_number())
-# print(ebyte.read_parameters())
-
 print(ebyte.read_parameters())
-
-print(ebyte._parameters)
 
 ebyte.transmission_mode = 1
 ebyte.address = 3
 ebyte.chan = 2
+
 print(ebyte.transmission_mode)
 print(ebyte.chan)
 print(ebyte.address)
 
 ebyte.write_parameters()
 
-# print(ebyte._parameters)
 print(ebyte.read_parameters())
