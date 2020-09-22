ee5a89c371f527b418d22f0bdcd2df801397698c
Thomas Schüßler <v@atomar.de>
2020-09-22 16:02:10 +0100

    WIP

--- test_raspberrypi.py
+++ test_raspberrypi.py
@@ -1,6 +1,5 @@
 import serial
 from ebyte import EbyteRaspberryPi
-from time import sleep
 
 PIN_M0 = 27
 PIN_M1 = 17
@@ -14,7 +13,7 @@ print(ebyte.read_version_number())
 print(ebyte.read_parameters())
 
 ebyte.transmission_mode = 1
-ebyte.address = 3
+ebyte.address = 2
 ebyte.chan = 2
 
 print(ebyte.transmission_mode)
